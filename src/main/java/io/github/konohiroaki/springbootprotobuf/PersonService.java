0fce4913d55b0cc31d7df9259acb20b90b402d6f
Hiroaki Kono <hiroakikono1990@gmail.com>
2018-11-11 22:04:51 +0900

    add grpc: stream request single response single request stream response stream request stream response

--- src/main/java/io/github/konohiroaki/springbootprotobuf/PersonService.java
+++ src/main/java/io/github/konohiroaki/springbootprotobuf/PersonService.java
@@ -2,6 +2,10 @@ package io.github.konohiroaki.springbootprotobuf;
 
 import org.lognet.springboot.grpc.GRpcService;
 
+import java.util.Map;
+
+import io.github.konohiroaki.sbp.Empty;
+import io.github.konohiroaki.sbp.Existence;
 import io.github.konohiroaki.sbp.Person;
 import io.github.konohiroaki.sbp.PersonServiceGrpc;
 import io.grpc.stub.StreamObserver;
@@ -15,6 +19,9 @@ public class PersonService extends PersonServiceGrpc.PersonServiceImplBase {
 
     private PersonRepository personRepo;
 
+    /**
+     * Get a person
+     */
     @Override
     public void getPerson(Person.PersonId request, StreamObserver<Person> responseObserver) {
         // Construct Person for response.
@@ -24,5 +31,68 @@ public class PersonService extends PersonServiceGrpc.PersonServiceImplBase {
         responseObserver.onNext(person);
         // Use .onCompleted() to respond.
         responseObserver.onCompleted();
+    }
+
+    /**
+     * Get all persons
+     */
+    @Override
+    public void listPersons(Empty request, StreamObserver<Person> responseObserver) {
+        for (Map.Entry<Person.PersonId, Person> person : personRepo.persons.entrySet()) {
+            System.out.println("[Server] Responding person: " + person.getKey().getId());
+            responseObserver.onNext(person.getValue());
+        }
+        responseObserver.onCompleted();
+    }
+
+    /**
+     * Check if all person exists
+     */
+    @Override
+    public StreamObserver<Person.PersonId> allPersonExisting(StreamObserver<Existence> responseObserver) {
+        return new StreamObserver<Person.PersonId>() {
+            boolean exists = true;
+
+            @Override public void onNext(Person.PersonId personId) {
+                System.out.println("[Server] Received request: " + personId.getId());
+                if (!personRepo.persons.containsKey(personId)) {
+                    exists = false;
+                }
+            }
+
+            @Override public void onError(Throwable throwable) {
+                System.out.println("[Server] Error: " + throwable.getMessage());
+            }
+
+            @Override public void onCompleted() {
+                System.out.println("[Server] onCompleted is called");
+                responseObserver.onNext(Existence.newBuilder().setExist(exists).build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    /**
+     * Get person for each requested personId
+     */
+    @Override
+    public StreamObserver<Person.PersonId> getPersons(StreamObserver<Person> responseObserver) {
+        return new StreamObserver<Person.PersonId>() {
+            @Override public void onNext(Person.PersonId personId) {
+                System.out.println("[Server] Received request: " + personId.getId());
+                if (personRepo.persons.containsKey(personId)) {
+                    responseObserver.onNext(personRepo.getPerson(personId));
+                }
+            }
+
+            @Override public void onError(Throwable throwable) {
+                System.out.println("[Server] Error: " + throwable.getMessage());
+            }
+
+            @Override public void onCompleted() {
+                System.out.println("[Server] onCompleted is called");
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
--- src/test/java/io/github/konohiroaki/springbootprotobuf/PersonServiceTest.java
+++ src/test/java/io/github/konohiroaki/springbootprotobuf/PersonServiceTest.java
@@ -7,10 +7,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
+import io.github.konohiroaki.sbp.Empty;
+import io.github.konohiroaki.sbp.Existence;
 import io.github.konohiroaki.sbp.Person;
 import io.github.konohiroaki.sbp.PersonServiceGrpc;
+import io.grpc.stub.StreamObserver;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootProtobufApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,5 +36,76 @@ public class PersonServiceTest extends GrpcServerTestBase {
         ListenableFuture<Person> personFuture = PersonServiceGrpc.newFutureStub(channel)
                 .getPerson(Person.PersonId.newBuilder().setId(1).build());
         System.out.println(personFuture.get());
+    }
+
+    @Test
+    public void listPersons() {
+        Iterator<Person> persons = PersonServiceGrpc.newBlockingStub(channel)
+                .listPersons(Empty.getDefaultInstance());
+        List<Person> personList = new ArrayList<>();
+        persons.forEachRemaining(personList::add);
+        List<Integer> idList = personList.stream().map(p -> p.getId().getId()).collect(Collectors.toList());
+
+        System.out.println("[Client] Received Response: " + idList);
+    }
+
+    @Test
+    public void allPersonExisting() throws InterruptedException {
+        CountDownLatch finishLatch = new CountDownLatch(1);
+        StreamObserver<Existence> responseObserver = new StreamObserver<Existence>() {
+            @Override public void onNext(Existence existence) {
+                System.out.println("[Client] Response from server was: " + existence.getExist());
+            }
+
+            @Override public void onError(Throwable throwable) {
+                System.out.println("[Client] Error: " + throwable.getMessage());
+                finishLatch.countDown();
+            }
+
+            @Override public void onCompleted() {
+                System.out.println("[Client] onCompleted is called");
+                finishLatch.countDown();
+            }
+        };
+        StreamObserver<Person.PersonId> requestObserver =
+                PersonServiceGrpc.newStub(channel).allPersonExisting(responseObserver);
+        for (int i = 1; i <= 2; i++) {
+            Person.PersonId personId = Person.PersonId.newBuilder().setId(i).build();
+            requestObserver.onNext(personId);
+            System.out.println("[Client] Sent request: " + personId.getId());
+        }
+        requestObserver.onCompleted();
+        System.out.println("[Client] Informed request completed");
+        finishLatch.await();
+    }
+
+    @Test
+    public void getPersons() throws InterruptedException {
+        CountDownLatch finishLatch = new CountDownLatch(1);
+        StreamObserver<Person> responseObserver = new StreamObserver<Person>() {
+            @Override public void onNext(Person person) {
+                System.out.println("[Client] Got response: " + person.getName());
+            }
+
+            @Override public void onError(Throwable throwable) {
+                System.out.println("[Client] Error: " + throwable.getMessage());
+                finishLatch.countDown();
+            }
+
+            @Override public void onCompleted() {
+                System.out.println("[Client] onCompleted is called");
+                finishLatch.countDown();
+            }
+        };
+        StreamObserver<Person.PersonId> requestObserver =
+                PersonServiceGrpc.newStub(channel).getPersons(responseObserver);
+        for (int i = 1; i <= 2; i++) {
+            Person.PersonId personId = Person.PersonId.newBuilder().setId(i).build();
+            requestObserver.onNext(personId);
+            System.out.println("[Client] Sent request: " + personId.getId());
+        }
+        requestObserver.onCompleted();
+        System.out.println("[Client] Informed request completed");
+        finishLatch.await();
     }
 }
